c8f591ddb094989a01bf4f53440d97c0efb61719
PramodPatil03 <pramodpatil190203@gmail.com>
2024-07-30 22:22:29 +0530

    update: UI

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,16 +9,16 @@ function Contact() {
       <div className='name'><p>Pramod Patil</p></div>
       <div className='contactContainer'>
         <div className='number'>
-          <span className="lable"><a href="tel:+918970627081">Mobile<span className="hidden">{': '}+91 8970627081</span></a></span>
+          <span className="lable"><a className='acontact' href="tel:+918970627081">Mobile<span className="hidden">{': '}+91 8970627081</span></a></span>
         </div>
         <div className='number'>
-          <span className="lable"><a target='_blank' href={mailtoHref}>Email<span className="hidden" >{': '} pramodpatil190203@gmail.com</span></a></span>
+          <span className="lable"><a  className='acontact' target='_blank' href={mailtoHref}>Email<span className="hidden" >{': '} pramodpatil190203@gmail.com</span></a></span>
         </div>
         <div className='number'>
-          <span className="lable"><a target='_blank' href="https://www.linkedin.com/in/pramod-patil-768706282/">LinkedIn<span className="hidden">{': '}Pramod Patil @ LinkedIn</span></a></span>
+          <span className="lable"><a  className='acontact' target='_blank' href="https://www.linkedin.com/in/pramod-patil-768706282/">LinkedIn<span className="hidden">{': '}Pramod Patil @ LinkedIn</span></a></span>
         </div>
         <div className='number'>
-          <span className="lable"><a target='_blank' href="https://github.com/PramodPatil03">GitHub<span className="hidden">{': '}Pramod Patil @ github</span></a></span>
+          <span className="lable"><a  className='acontact' target='_blank' href="https://github.com/PramodPatil03">GitHub<span className="hidden">{': '}Pramod Patil @ github</span></a></span>
         </div>
       </div>
     </div>
